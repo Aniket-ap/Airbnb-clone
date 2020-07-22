d172a52517a6e7950419c6d30171a52efdde0a16
Aniket Pradhan <aniket_pro@outlook.com>
2020-07-22 19:24:37 +0530

    airbnb

--- airbnb-clone/src/utility/NavBar/NavBar.js
+++ airbnb-clone/src/utility/NavBar/NavBar.js
@@ -9,6 +9,12 @@ import SignUp from '../../pages/Login/SignUp'
 
 class NavBar extends Component{
 
+    componentDidUpdate(oldProps){
+        if(oldProps.auth.token !== this.props.auth.token){
+            this.props.openModal('closed','');
+        }
+    }
+
     render(){
 
         let navColor = 'transparent';
@@ -21,14 +27,22 @@ class NavBar extends Component{
             <div className="row">
                 <nav className={navColor}>
                     <div className="nav-wrapper">
-                        <Link to="/" className="left">AIRBNB</Link>
+                        <Link to="/" className="left">airbnb</Link>
                         <ul id="nav-mobile" className="right">
                             <li><Link to="/">English (US)</Link></li>
                             <li><Link to="/">$ USD</Link></li>
                             <li><Link to="/">Become a host</Link></li>
                             <li><Link to="/">Help</Link></li>
-                            <li className="login-signup" onClick={()=>{this.props.openModal('open',<SignUp />)}}>Sign Up</li>
-                            <li className="login-signup" onClick={()=>{this.props.openModal('open',<Login />)}}>Log in</li>
+                            {this.props.auth.email
+                                ?   <>
+                                        <li>Hello, {this.props.auth.email}</li>
+                                        <li>Logout</li>
+                                    </>
+                                :   <>
+                                        <li className="login-signup" onClick={()=>{this.props.openModal('open',<SignUp />)}}>Sign Up</li>
+                                        <li className="login-signup" onClick={()=>{this.props.openModal('open',<Login />)}}>Log in</li>
+                                    </>
+                            }
                         </ul>
                     </div>
                 </nav>
@@ -38,10 +52,16 @@ class NavBar extends Component{
     }
 }
 
+function mapStateToProps(state){
+    return{
+        auth: state.auth,
+    }
+}
+
 function mapDispatchToProps(dispatcher){
     return bindActionCreators({
         openModal: openModal
     },dispatcher)
 }
 
-export default connect(null, mapDispatchToProps)(NavBar);
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
